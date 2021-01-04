27b5831f742b98a1c55917380a48c38d0e8afa0f
Patrick Peek <patrick.peek@pacurro.nl>
2021-01-04 07:55:46 +0100

    corrected docker username

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
 
-docker build -t peekp/multi-client:latest -t peekp/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t peekp/multi-server:latest -t peekp/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t peekp/multi-worker:latest -t peekp/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t patrickpeek/multi-client:latest -t patrickpeek/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t patrickpeek/multi-server:latest -t patrickpeek/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t patrickpeek/multi-worker:latest -t patrickpeek/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push peekp/multi-client:latest
-docker push peekp/multi-server:latest
-docker push peekp/multi-worker:latest
+docker push patrickpeek/multi-client:latest
+docker push patrickpeek/multi-server:latest
+docker push patrickpeek/multi-worker:latest
 
-docker push peekp/multi-client:$SHA
-docker push peekp/multi-server:$SHA
-docker push peekp/multi-worker:$SHA
+docker push patrickpeek/multi-client:$SHA
+docker push patrickpeek/multi-server:$SHA
+docker push patrickpeek/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=peekp/multi-server:$SHA
-kubectl set image deployments/client-deployment client=peekp/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=peekp/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=patrickpeek/multi-server:$SHA
+kubectl set image deployments/client-deployment client=patrickpeek/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=patrickpeek/multi-worker:$SHA
